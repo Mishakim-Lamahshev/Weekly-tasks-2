15b4ceb94197f0460d2ce164d12311e0bf1bb1c3
Yonatan Tal <talyonatantal@gmail.com>
2024-01-14 20:51:03 +0200

    Section B.3 completed

---
+++ Assets/Player1.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Player1Controller : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(horizontal, vertical) * speed * Time.deltaTime;
+        transform.Translate(movement);
+        if(Mathf.Abs(transform.position.x) > 29.35f)
+        {
+            transform.position = new Vector3(transform.position.x*-1, transform.position.y, transform.position.z);
+        }
+        if(Mathf.Abs(transform.position.y) > 7.9f)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y*-1, transform.position.z);
+        }
+    }
+}
---
+++ Assets/Player2.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Controller : MonoBehaviour
+{
+    float speed = 5.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        { 
+            transform.Translate(Vector3.down * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.A)) { 
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+        if(Mathf.Abs(transform.position.x) > 29.35f)
+        {
+            transform.position = new Vector3(transform.position.x*-1, transform.position.y, transform.position.z);
+        }
+        if(Mathf.Abs(transform.position.y) > 7.9f)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y*-1, transform.position.z);
+        }
+    }
+}
