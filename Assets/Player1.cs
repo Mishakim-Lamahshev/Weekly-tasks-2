5061a385049c00377992bf50ef0a41dd6da33f4c
Yonatan Tal <talyonatantal@gmail.com>
2024-01-17 01:20:32 +0200

    check

--- Assets/Player1.cs
+++ Assets/Player1.cs
@@ -10,7 +10,15 @@ public class Player1Controller : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(horizontal, vertical) * speed * Time.deltaTime;
         transform.Translate(movement);
-        if(Mathf.Abs(transform.position.x) > 29.35f){transform.position = new Vector3(transform.position.x*-1, transform.position.y, transform.position.z);}
-        if(Mathf.Abs(transform.position.y) > 7.9f){transform.position = new Vector3(transform.position.x, transform.position.y*-1, transform.position.z);}
+
+        if (Mathf.Abs(transform.position.x) > 29.35f)
+        {
+            transform.position = new Vector3(transform.position.x * -1, transform.position.y, transform.position.z);
+        }
+
+        if (Mathf.Abs(transform.position.y) > 7.9f)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y * -1, transform.position.z);
+        }
     }
 }
