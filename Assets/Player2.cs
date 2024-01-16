16440cb1587137d2de12fa1ba200d0024812c4bf
Yonatan Tal <talyonatantal@gmail.com>
2024-01-17 01:32:06 +0200

    Update Player2.cs

--- Assets/Player2.cs
+++ Assets/Player2.cs
@@ -14,12 +14,10 @@ public class Player2Controller : MonoBehaviour
             transform.Translate(Vector3.up * Time.deltaTime * speed);
         }
         if (Input.GetKey(KeyCode.S))
-        { 
-            transform.Translate(Vector3.down * Time.deltaTime * speed);
+        {transform.Translate(Vector3.down * Time.deltaTime * speed);
         }
         if (Input.GetKey(KeyCode.A))
-        { 
-            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        {transform.Translate(Vector3.left * Time.deltaTime * speed);
         }
         if (Input.GetKey(KeyCode.D))
         {
